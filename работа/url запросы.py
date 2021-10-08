55840a0718418343b0575904af8573cf8627f767
pounson <fakhradg@gmail.com>
2021-10-08 17:45:29 +0300

    "test"

---
+++ работа/url запросы.py
@@ -0,0 +1,12 @@
+import requests
+TOKEN = "2619421814940190"
+
+urls = "https://superheroapi.com/api/2619421814940190/search/"
+superheroes = [{'name': 'Hulk'}, {'name': 'Captain America'}, {'name': 'Thanos'}]
+
+for hero in superheroes:
+    hero01 = requests.get(urls + hero['name'])
+    hero['intelligence'] = int(hero01.json()['results'][0]['powerstats']['intelligence'])
+
+print(sorted(superheroes, key=lambda hero: -hero['intelligence'])[0]['name'])
+
